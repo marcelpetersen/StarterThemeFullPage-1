39ae4d7ed9d2caec22ee071c6a163231a7e9ba61
Rona <ronalochner@gmail.com>
2018-03-08 10:26:41 +0200

    FullPage.Js Starter theme
    This is the stripped down Fizz website. If you need an Instagram feed go check out those files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    menu: '#menu',
+    lockAnchors: true,
+    fitToSection: false,
+    autoScrolling: true,
+    scrollOverflow: true,
+  });
+});
